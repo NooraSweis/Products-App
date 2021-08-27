232ad7b9ef3bb8e811db9bad495d40a028f1ccb1
NooraSweis <noora.sweis@yahoo.com>
2021-08-27 19:50:53 +0300

    insert into product table

--- routes/product.js
+++ routes/product.js
@@ -7,8 +7,30 @@ module.exports = {
     },
 
     addProduct: (req, res) => {
+        if (!req.files) {
+            return res.status(400).send("No Product Image Uploaded");
+        }
+        let message = '',
+            title = req.body.title,
+            category = req.body.category,
+            description = req.body.description,
+            quantity = req.body.quantity,
+            price = req.body.price,
+            UploadedFile = req.files.image;
+
+        let insertQuery = "INSERT INTO product (title,quantity,"
+            + " price, category, image, description)"
+            + ` VALUES ('${title}', ${quantity}, ${price}, '${category}',`
+            + ` '${UploadedFile.name}', '${description}');`;
+        pool.query(insertQuery, (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).json(results.rows)
+        });
+
         res.render("add_product.ejs", {
-            message: "Not Working yet",
+            message: insertQuery,
             title: "Add Product"
         });
     }
